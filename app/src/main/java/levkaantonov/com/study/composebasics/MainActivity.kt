d2f86892f1c2f16054ed1a3df578e3a4c5730d7c
levkaantonov <levkaantonov@gmail.com>
2021-11-30 13:43:59 +0300

    simple example

--- app/src/main/java/levkaantonov/com/study/composebasics/MainActivity.kt
+++ app/src/main/java/levkaantonov/com/study/composebasics/MainActivity.kt
@@ -3,14 +3,18 @@ package levkaantonov.com.study.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import levkaantonov.com.study.composebasics.ui.OnBoardingScreen
 import levkaantonov.com.study.composebasics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,23 +29,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+private fun Greeting(
+    name: String
+) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(24.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello,")
+                Text(text = "$name!")
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value },
+            ) {
+                Text(text = if (expanded.value) "Show less" else "Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposeBasicsTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
 
 @Composable
-private fun MyApp() {
-    Surface(color = MaterialTheme.colors.primary) {
-        Greeting("Android")
+fun MyApp() {
+    var shouldShowOnBoarding by remember {
+        mutableStateOf(true)
+    }
+
+    if (shouldShowOnBoarding) {
+        OnBoardingScreen(
+            onContinueClicked = { shouldShowOnBoarding = false }
+        )
+    } else {
+        Greetings()
     }
 }
