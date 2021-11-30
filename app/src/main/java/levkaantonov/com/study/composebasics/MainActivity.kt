7116d2f9757e5121e36ba4a7f076dc3536e5cfcb
levkaantonov <levkaantonov@gmail.com>
2021-11-30 16:01:49 +0300

    complete codelab

--- app/src/main/java/levkaantonov/com/study/composebasics/MainActivity.kt
+++ app/src/main/java/levkaantonov/com/study/composebasics/MainActivity.kt
@@ -1,17 +1,31 @@
 package levkaantonov.com.study.composebasics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import levkaantonov.com.study.composebasics.ui.OnBoardingScreen
@@ -32,41 +46,91 @@ class MainActivity : ComponentActivity() {
 private fun Greeting(
     name: String
 ) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
     Surface(
         color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp)
+        modifier = Modifier
+            .padding(horizontal = 4.dp, vertical = 4.dp)
+            .clip(RoundedCornerShape(16.dp))
     ) {
         Row(
-            modifier = Modifier.padding(24.dp)
+            modifier = Modifier
+                .padding(24.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
         ) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(12.dp)
             ) {
                 Text(text = "Hello,")
-                Text(text = "$name!")
+                Text(
+                    text = "$name", style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+                if (expanded) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4)
+                    )
+                }
             }
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value },
+            IconButton(
+                onClick = { expanded = !expanded },
             ) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
+                Icon(
+                    imageVector = if (expanded)
+                        Icons.Filled.ExpandLess else
+                        Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded)
+                        stringResource(R.string.button_text_show_less) else
+                        stringResource(R.string.button_text_show_more)
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(
+        modifier = Modifier
+            .padding(vertical = 4.dp)
+    ) {
+        items(names) { name ->
             Greeting(name = name)
         }
     }
 }
 
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingsPreview() {
+    ComposeBasicsTheme {
+        Greetings()
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
@@ -77,7 +141,7 @@ fun DefaultPreview() {
 
 @Composable
 fun MyApp() {
-    var shouldShowOnBoarding by remember {
+    var shouldShowOnBoarding by rememberSaveable {
         mutableStateOf(true)
     }
 
--- app/src/main/java/levkaantonov/com/study/composebasics/ui/theme/Color.kt
+++ app/src/main/java/levkaantonov/com/study/composebasics/ui/theme/Color.kt
@@ -3,6 +3,10 @@ package levkaantonov.com.study.composebasics.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
+val Purple500 = Color(0xFF1976D2)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val Navy = Color(0XFF073042)
+val Blue = Color(0XFF4285F4)
+val LightBlue = Color(0XFFD7EFFE)
+val Chartreuse = Color(0XFFEFF7CF)
